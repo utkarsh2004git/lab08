21b8b1df9c28737682a7df6eefc64d4d6ba13dc9
ukarsh2004git <utkarshshahare2004@gmail.com>
2023-12-09 19:02:46 +0530

    Okay!

---
+++ database/migrations/2023_11_22_132716_create_people_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('people', function (Blueprint $table) {
+            $table->text('title');
+            $table->text('first_name');
+            $table->text('last_name');
+            $table->text('country');
+            $table->text('email');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('people');
+    }
+};
